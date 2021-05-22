f7ffb0509486dffa450e0db0b13487a9d71bb80c
Prem Kamal Osipalli <kamalosipalli@gmail.com>
2021-05-22 17:35:08 +0530

    changed SignIn and SignUp UI

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,23 +6,14 @@ import android.os.Bundle
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var signIn: Button;
     lateinit var signUp: Button;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        signIn=findViewById(R.id.gotoSignIn)
         signUp=findViewById(R.id.gotoSignUp)
 
-        signIn.setOnClickListener(){
 
-            val intent:Intent = Intent(applicationContext, SignIn::class.java)
-
-            startActivity(intent)
-        }
 
         signUp.setOnClickListener(){
 
