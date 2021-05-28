f7d7b90618154c295823e6bb653a91da208c2c59
Prem Kamal Osipalli <kamalosipalli@gmail.com>
2021-05-28 23:30:53 +0530

    Activities created

--- app/src/main/java/com/example/myapplication/TakeSurvey.kt
+++ app/src/main/java/com/example/myapplication/TakeSurvey.kt
@@ -1,11 +1,23 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class TakeSurvey : AppCompatActivity() {
+    private lateinit var submit:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_take_survey)
+
+        submit=findViewById(R.id.submit)
+
+        submit.setOnClickListener(){
+            val intent: Intent = Intent(applicationContext, CovidTest::class.java)
+
+            startActivity(intent)
+        }
+
     }
 }
